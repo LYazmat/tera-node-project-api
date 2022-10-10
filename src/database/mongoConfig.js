513c2604db0a2b1f20c32d9fec1d9f3d47507c71
LYazmat <marcusvmiranda@live.com>
2022-10-10 11:42:00 -0300

    configs change

--- src/database/mongoConfig.js
+++ src/database/mongoConfig.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const MONGODB_URI = process.env.MONGODB_ATLASURI
+const MONGODB_URI = process.env.MONGODB_ATLASURI;
 
-const connect = async() => {
-    try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log('Banco conectado')
-    } catch (error) {
-        console.error('Erro: ', error.message)
-    }
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Banco conectado");
+  } catch (error) {
+    console.error("Erro: ", error.message);
+  }
+};
 
 module.exports = {
-    connect
-}
+  connect,
+};
